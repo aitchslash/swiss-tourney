29b5c819a845944ef144400b510e0d9ef455be35
Benjamin Field <benjamin.field@gmail.com>
2015-02-23 14:35:56 -0500

    Clean tons of extra code

--- tournament.py
+++ tournament.py
@@ -5,11 +5,11 @@
 
 # ---- to do list ----
 #   bye implementation - if count is even addPlayer
+#   tie implementation
 #   cursor.execute(open("schema.sql", "r").read())
 #   newstr = "".join(oldstr.split('\n'))
 
 import psycopg2
-
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -94,48 +94,21 @@ def reportMatch(winner, loser, draw=False, tourneyID=1):
     else:
         print "Two players are needed for a match"
     
-def swissPairings2():
-    standings = playerStandings()
-    
-    # get prior match-ups from results
-    results = connect2('select winner, loser from results', True)
-    
-    # get round number, assume everyone has played 
-    #   equal number of games before swissPairings can be called
-    roundNumber = standings[0][3] + 1
-    numMatches = countPlayers() / 2
-    # generate trivial solution from standings
-    #   with no dupes it's maximized 
-    pairings = []
-    for i in range(0, numMatches):
-        pairings.append((standings[i*2][0], standings[i*2][1], 
-            standings[i*2+1][0], standings[i*2+1][1]))
-    # duplicate matchups impossible for rounds <= 2
-    if roundNumber <= 2:
-        return pairings
-    # for later rounds need to check for duplicates
-    
-    # create disallowed list
-    disallowed = []
-    for matchup in results:
-        disallowed.append(matchup)
-        disallowed.append((matchup[1], matchup[0]))
-
-    # loop through pairings checking for duplicates
-    dupes = False
-    for pairing in pairings:
-        if (pairing[0], pairing[2]) in disallowed:
-            dupes = True
-    if dupes == False:
-        return pairings
-    else:
-        print "DUPES!!!!"
-        # get player list (w/ points?)
-        # make pairs (if pts, could evaluate here?)
-        # evaluate 
-    return pairings
-
-def swissPairings3():
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
+  
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+  
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
     standings = playerStandings()
     
     # get round number, assume everyone has played 
@@ -154,45 +127,12 @@ def swissPairings3():
         return pairings
     # for later rounds need to check for duplicates
     else:
-        # get prior match-ups from results
-        #results = connect2('select winner, loser from results', True)
         bestPairTuples = getBestPairings()
-        # get name: connect2('select name from players where playerID=%s' % (200), True)
+        # need to get in correct format
         for pair in bestPairTuples:
             pairings.append([pair[0], nameFromID(pair[0]),
                             pair[1], nameFromID(pair[1])])
         return pairings
-
-'''    
-        # create disallowed list
-        disallowed = []
-        for matchup in results:
-            disallowed.append(matchup)
-            disallowed.append((matchup[1], matchup[0]))
-
-        # loop through pairings checking for duplicates
-        dupes = False
-        for pairing in pairings:
-            if (pairing[0], pairing[2]) in disallowed:
-                dupes = True
-        if dupes == False:
-            return pairings
-        else:
-            print "DUPES!!!!"
-            # get player list (w/ points?)
-            # make pairs (if pts, could evaluate here?)
-            # evaluate
-            ''' 
-    #return pairings
-
-def generatePairs(players):
-    a = players[0]
-    holder = []
-    for i in range(1,len(players)): # could call count players
-        pairOne = (a, players[0])
-        holder = [pairOne]
-        for remainder in generatePairs(players[1:i]+players[i+1:]):
-            holder.append(remainder)
 
 # will have to alter for ties, but good 
 def makePointsDict():
@@ -245,7 +185,6 @@ def makePlayerList():
         playerList.append(item[0])
     return playerList
 
-# alternate implementation using a generator
 #   count call count players
 def genPairs(playerList):
     if len(playerList) < 2:
@@ -278,58 +217,3 @@ def checkDupes(pairings):
         return pairings
     else:
         print "DUPES"
- 
-def swissPairings():
-    """Returns a list of pairs of players for the next round of a match.
-  
-    Assuming that there are an even number of players registered, each player
-    appears exactly once in the pairings.  Each player is paired with another
-    player with an equal or nearly-equal win record, that is, a player adjacent
-    to him or her in the standings.
-  
-    Returns:
-      A list of tuples, each of which contains (id1, name1, id2, name2)
-        id1: the first player's unique id
-        name1: the first player's name
-        id2: the second player's unique id
-        name2: the second player's name
-    """
-    # check if even number - only for extra credit - do later
-    # countPlayers()
-    # if odd
-    # registerPlayer "bye" # this would/shoud only happen in the first round
-    #   might want to update standings....
-    #
-    # design pairings
-    # no problem at top or bottom - all wins and all losses will never
-    #   have played one another
-    # ooh, prepare for ties - 3 pts/w, 1pt/t maybe
-    #   and perhaps enhanced rankings too
-    #
-    # grab count
-    # derive total rounds from count
-    # grab wins from standings
-    # rounds completed is top player's win count
-    # round 1 is win count = 0, etc
-    # sort players by id - maybe not necessary
-    # grab playerID's if not done yet
-    
-    # if rounds = 1 or 0, no worries
-    # no worries where wins = 0 or wins = rounds completed
-    # fire those off
-    # if none remain done
-    # deal with the rest
-
-    # or brute force
-    standings = playerStandings()
-    # get prior match-up from results
-    results = connect2('select winner, loser from results', True)
-    
-    numMatches = countPlayers() / 2
-    pairings = []
-    for i in range(0, numMatches):
-        pairings.append((standings[i*2][0], standings[i*2][1], 
-            standings[i*2+1][0], standings[i*2+1][1]))
-    return pairings
-    
-
