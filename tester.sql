bc5c2ee09d1180239c8347abdd45c95647eb5292
Benjamin Field <benjamin.field@gmail.com>
2015-02-27 08:39:45 -0500

    Update .sql test file

--- tester.sql
+++ tester.sql
@@ -3,18 +3,16 @@ drop view if exists ties cascade;
 drop view if exists losses cascade;
 drop view if exists wins;
 
-
 create view wins as select winner as id, count(*) as wins  
-	from results where draw = false group by winner;
+	from results where draw = false AND tourneyID=%s group by winner;
 
 create view losses as select loser as id, count(*) as losses 
-	from results where draw = false group by loser;
+	from results where draw = false AND tourneyID=%s group by loser;
 
 select 
 	players.playerid as id, 
 	players.name,
 	coalesce(wins.wins, 0) as w, 
-	-- coalesce(losses.losses, 0) as l,
 	(coalesce(wins.wins, 0) + coalesce(losses.losses, 0)) as gp
 from 
 	players 
